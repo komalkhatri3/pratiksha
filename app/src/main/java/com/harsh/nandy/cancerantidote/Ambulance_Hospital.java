3480625b6791fbe8f1a974a281340bfd52461620
popla <komalkhatri03021996@gmail.com>
2017-03-26 09:45:20 -0600

    modified

--- app/src/main/java/com/harsh/nandy/cancerantidote/Ambulance_Hospital.java
+++ app/src/main/java/com/harsh/nandy/cancerantidote/Ambulance_Hospital.java
@@ -1,14 +1,23 @@
 package com.harsh.nandy.cancerantidote;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ambulance_Hospital extends AppCompatActivity {
     DatabaseHelper databseHelper;
+    SQLiteDatabase sqLiteDatabase;
+    Cursor cursor;
 
     Spinner mySpinner;
 
@@ -17,22 +26,53 @@ public class Ambulance_Hospital extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambulance__hospital);
         databseHelper=new DatabaseHelper(this);
+        ListView listView= (ListView) findViewById(R.id.listview1);
         mySpinner = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Ambulance_Hospital.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.state_name));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sqLiteDatabase=databseHelper.getReadableDatabase();
+                final List<Ambulance> AmbulanceAvail= new ArrayList<Ambulance>();
+                Log.d("MESSAGE","In onItemSelected ");
+                cursor = databseHelper.getdata(mySpinner.getSelectedItem().toString());
+                if(cursor.moveToFirst())
+                    do
+                    {
+                        Ambulance ambulance =new Ambulance();
+                        ambulance.setHospital(cursor.getString(0));
+                        ambulance.setState(cursor.getString((1)));
+                        ambulance.setAvailable(cursor.getInt(2));
+                        AmbulanceAvail.add(ambulance);
+
+                    }while (cursor.moveToNext());
+                Adaptor adaptor;
+                adaptor = new Adaptor(this,AmbulanceAvail);
+
                 switch (position)
                 {
                     case 1:
+
+                             String Hospital,avail;
+
+
+
+
                         break;
                     case 2:
+
                         break;
                     case 3:
+
                         break;
                     case 4:
+
                         break;
                      default:
                 }
--- app/src/main/java/com/harsh/nandy/cancerantidote/DatabaseHelper.java
+++ app/src/main/java/com/harsh/nandy/cancerantidote/DatabaseHelper.java
@@ -1,10 +1,9 @@
 package com.harsh.nandy.cancerantidote;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import static android.R.attr.version;
 
 /**
  * Created by popla on 25-03-2017.
@@ -20,7 +19,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE"+TABLE_NAME+"(ID PRIMARY KEY AUTOINCREMENT,HOSPITALS TEXT,STATE TEXT,AVAIL_AMBULANCE INTEGER)");
+        db.execSQL("CREATE TABLE"+TABLE_NAME+"(ID PRIMARY KEY AUTOINCREMENT,HOSPITALS TEXT,STATE TEXT,AVAIL_AMBULANCE TEXT)");
          onCreate(db);
     }
 
@@ -28,4 +27,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+    public Cursor getdata(String state)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        String query  ="SELECT * FROM"+TABLE_NAME+"WHERE STATE='"+state+"'";
+        Cursor cursor=db.rawQuery(query,null);
+
+        return cursor;
+    }
+
 }
